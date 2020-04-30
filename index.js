0edc107d19a75dcce58fd4497f5eb9bc139dd2dc
Kristalkill <vovk.fox@gmail.com>
2020-04-30 20:44:23 +0300

    0.2

--- index.js
+++ index.js
@@ -9,89 +9,104 @@ global.mongoose = require("mongoose");
 
 ///____CONST____////
 const Main = new Discord.Client({ disableEveryone: true });
-const command = new Discord.Collection()
+const command = new Discord.Collection();
 const aliases = new Discord.Collection();
+const randomize = function(min, max) {
+    return Math.floor(Math.random() * (max-min) + min)
+};
 ////____GLOBAL____///
 global.Main = Main;
-global.user = require('./models/user.js')
-global.guild = require('./models/guild.js')
-global.clan = require('./models/clan.js')
-global.config = require('./config.json')
-global.Owner = guild.ownerID;
+global.User = require('./models/user.js');
+global.Guild = require('./models/guild.js');
+global.Clan = require('./models/clan.js');
+global.config = require('./config.json');
+global.Owner = Guild.ownerID;
 ///____ENV____///
 process.env.Token;
 process.env.youtubeToken;
 //____MAIN____///
 Main.colors = require("./color.json");
-Main.clan = clan;
-Main.guild = guild;
-Main.user = user;
+Main.clan = Clan;
+Main.guild = Guild;
+Main.user = User;
+Main.commands = command;
+Main.aliases = aliases;
 ///____Export______///
 mongoose.connect(config.dataURL,{ useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('connected',()=>{
   console.log('[✅DataBase] Connected!')
-})
-async (bot, mssage, args) => {
-Main.guild.findOne({guildID:message.guild.id}, (err,res) => {
+});
+Main.sendHelp = function (cmd, message) {
+    Main.commands.get('help').run(Main, message, [cmd])
+}
+fs.readdir("./commands/", (err, files) => {
+  if (err) bot.logsErr();
+
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+  console.log(`Загружено ${jsfiles.length} комманд`);
+
+  jsfiles.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    Main.commands.set(props.help.name, props);
+    if (!props.help.description) {
+      props.help.description = "Это секрет даже для разработчика";
+    }
+    if (!props.help.category) {
+      props.help.category = "Не сортированая команда";
+    }
+    if (!props.help.owneronly) {
+      props.help.owneronly = false;
+    }
+    props.help.aliases.forEach(alias => {
+      if (!props.help.name) return console.log(`в файле ${f} нет help.name`);
+      Main.aliases.set(alias, props.help.name);
+    });
+  });
+});
+Main.on('message', async(message) => {
+  if (!message.content.startsWith(Guild.prefix)) return;
+      const args = message.content.slice(Guild.prefix.length).trim().split(/ +/g);
+      const cmds = args.shift().toLowerCase();
+      const props = bot.commands.get(cmdName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmd));
+      if(!require('./config.json').owner.includes(message.author.id) && command.public === false) return;
+      command.execute(bot, message, args);
+});
+Main.on('message', async(message) => {
+     Guild.findOne({guildID:message.guild.id}, (err,Data) => {
     if(err) return message.channel.send(`[❌DataBase] Произошла ошибка при добавлении сервера в базу-данных`)
-    if(!res){
-      let guild = new Guild({guildID: message.guild.id})
+    if(!Data){
+      let guild = new Guild({guildID:message.guild.id})
       message.channel.send(`\`[✅DataBase]\` **${message.guild.name}** Успешно была добавлена в базу-данных`)
       guild.save().catch(err => message.channel.send(`\`[❌DataBase]\` Произошла ошибка при сохранении сервера в базу-данных. Ошибка: \`\`\`${err}\`\`\``));
 }
 })
-};
-exports.randomize = function(min,max){
- return Math.floor(Math.random()*(man-min)+ min);
- };
+});
  Main.on('message', async(message) => {
    if(message.author.bot) return;
-     user.findOne({guildID: message.guild.id, userID: message.author.id}, (err,res) => {
+    User.findOne({guildID: message.guild.id, userID: message.author.id}, (err,Data) => {
      if(err) return message.channel.send(`\`[❌DataBase]\` Произошла ошибка при добавлении пользователя в базу-данных`)
-     if(!res){
+     if(!Data){
        let user = new User({guildID: message.guild.id, userID: message.author.id})
        message.channel.send(`\`[✅DataBase]\` **${message.author.username}** Успешно был(а) добавлен в базу-данных`)
        user.save().catch(err => message.channel.send(`\`[❌DataBase]\` Произошла ошибка при сохранении данных в базу-данных. Ошибка: \`\`\`${err}\`\`\``));
      }else{
-    Data.user.xp += randomaze(guild.RandomizeMinXp,guild.RandomizeMaxXp);
-    Data.user.money += randomaze(guild.RandomizeMinMoney,guild.RandomizeMaxMoney);
-    Data.user.massages++
+    Data.xp += randomize(1,3);
+    Data.money += randomize(1,3);
+    Data.massages++
     Data.save()
-    if(User.xp > (Guild.upXp*Data.user.level)){
+    if(User.xp > (Guild.upXp*User.level)){
      let embed = new Discord.MessageEmbed()
       .setColor(color)
       .setTitle('Ура!')
-      .setDescription(`[:tada:] Поздравим **${message.author.username}** с ${Data.user.level} уровнем!`)
+      .setDescription(`[:tada:] Поздравим **${message.author.username}** с ${Data.User.level} уровнем!`)
       message.channel.send(embed)
-      Data.user.xp -= (Data.upXp*Data.user.level);
-      Data.user.level+=1
+      Data.xp -= (Guild.upXp*User.level);
+      Data.level+=1
       Data.save()
  }
 }
 })
 });
-
-fs.readdirSync('./commands').forEach(module => {
-    const commandFiles = fs.readdirSync(`./commands/${module}/`).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-
-        const command = require(`./commands/${module}/${file}`);
-        console.log(`Загружено ${commandFiles.length} комманд`);
-          if (!command.help.description) {
-            command.help.description == "Нету,напишите /report об этом";
-          }
-          if (!command.help.category) {
-            command.help.category == module;
-          }
-          if (!command.help.owneronly) {
-            command.help.owneronly == false;
-          }
-          command.help.aliases.forEach(alias => {
-            if (!command.help.name) return console.log(`в файле ${f} нет help.name`);
-            aliases.set(alias, command.help.name);
-          })
-        }
-    });
     Main.login(config.Token).catch(err => {
       if (err.message.toLowerCase().includes("incorrect login"))
         console.log(
--- models/guild.js
+++ models/guild.js
@@ -5,12 +5,12 @@ const guildchema = mongoose.Schema({
     linked:{type: Boolean,default: true},
     logs:{type: Boolean,default: true},
     clanPrice:{ type: Number, default: 15000 },
-    bonusInterval:{type:String,default: 1440},
-    bonus:{type:String,default:50},
+    bonusInterval:{type:Number,default: 1440},
+    bonus:{type:Number,default:50},
+    RandomizeMaxMoney:{type:String,default:4},
+    RandomizeMinMoney:{type:String,default:1},
     RandomizeMaxXp:{type:String,default:5},
     RandomizeMinXp:{type:String,default:1},
-    RandomizeMaxMoney:{type:String,default:3},
-    RandomizeMinMoney:{type:String,default:1},
-    upXP:{type:String,default:100}
+    upXP:{type:Number,default:100}
 });
 module.exports = mongoose.model("Guild", guildchema)
