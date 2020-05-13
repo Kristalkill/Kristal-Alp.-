070b90bc1f1a3d6235ddef6031de6b95e8dc0b8c
Kristalkill <vovk.fox@gmail.com>
2020-05-13 16:54:58 +0300

    Update indexs.js

--- indexs.js
+++ indexs.js
@@ -10,7 +10,7 @@ global.mongoose = require("mongoose");
 ////____FUNCTIONS___///
 const dotenv = require("dotenv");
 dotenv.config();
-port = process.env.PORT
+process.env.PORT
 addAchievement = require('./functions/addAchievement.js')
 ////____GLOBAL____///
 global.User = require('./models/user.js');
@@ -24,7 +24,7 @@ Main.colors = require("./color.json");
 Main.commands = new Discord.Collection();
 Main.aliases  = new Discord.Collection();
 ///____Export______///
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(`${process.env.MONGODB_URI}`, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('connected',()=>{
   console.log('[✅DataBase] Connected!')
 })
