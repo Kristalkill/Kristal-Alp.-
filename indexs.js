e23c40ab4ab6acffa34b2644aadf15262884345e
Kristalkill <vovk.fox@gmail.com>
2020-07-10 02:03:03 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -73,7 +73,6 @@ Main.on('messageUpdate', async(oldMsg, message) => {
   if(message.author.bot)return;
   User.findOne({guildID: message.guild.id, userID: message.author.id},(err,Data)=> {
   Guild.findOne({guildID: message.guild.id},(err,res) => {
-  Block.findOne({id: message.author.id},(err,BlockY)=> {
  if(err){console.log(err);}
  if(!Data){
     let user = new User({guildID:message.guild.id, userID:message.author.id})
@@ -107,26 +106,22 @@ const args = message.content.slice(res.Moderation.prefix.length).trim().split(/ 
 const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
-switch(BlockY){ 
-case true:{ message.react("⏪") }break;
-case false:{
+Block.findOne({id: message.author.id},(err,BlockY)=> {
+if(BlockY){
+  message.react("⏪"); 
+}return;
+})
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
 if(!config.owner.includes(message.author.id)&&(!message.guild.owner.user)&&(!member.hasPermission(command.Permission)))return message.reply(ErrEmbed.setDescription(`**У вас нету прав** ${command.Permission}`));
 command.execute(Main, message, args,res,Data,err);
 }
-}
-}
-}
-)
-})
 })
 })
 Main.on('message', async(message) => {  
   if(message.author.bot)return;
   User.findOne({guildID: message.guild.id, userID: message.author.id},(err,Data)=> {
   Guild.findOne({guildID: message.guild.id},(err,res) => {
-  Block.findOne({id: message.author.id},(err,BlockY)=> {
  if(err){console.log(err);}
  if(!Data){
     let user = new User({guildID:message.guild.id, userID:message.author.id})
@@ -160,19 +155,16 @@ const args = message.content.slice(res.Moderation.prefix.length).trim().split(/ 
 const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
-switch(BlockY){ 
-case true:{ message.react("⏪") }break;
-case false:{
+Block.findOne({id: message.author.id},(err,BlockY)=> {
+if(BlockY){
+  message.react("⏪"); 
+}return;
+})
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
 if(!config.owner.includes(message.author.id)&&(!message.guild.owner.user)&&(!member.hasPermission(command.Permission)))return message.reply(ErrEmbed.setDescription(`**У вас нету прав** ${command.Permission}`));
 command.execute(Main, message, args,res,Data,err);
 }
-}
-}
-}
-)
-})
 })
 })
 Main.on('ready', async () => {
