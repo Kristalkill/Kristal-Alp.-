b8bdd6df28b558fa53f19221b7039f6d401485df
Kristalkill <vovk.fox@gmail.com>
2020-07-10 02:09:09 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -107,9 +107,7 @@ const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
 Block.findOne({id: message.author.id},(err,BlockY)=> {
-if(BlockY){
-  message.react("⏪"); 
-}return;
+if(BlockY)return message.react("⏪");return; 
 })
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
@@ -157,9 +155,7 @@ const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
 Block.findOne({id: message.author.id},(err,BlockY)=> {
-if(BlockY){
-  message.react("⏪"); 
-}return;
+if(BlockY)return message.react("⏪");return; 
 })
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
