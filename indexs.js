1f58d58f3be6133a40c55fbe952bbccf11d12097
Kristalkill <vovk.fox@gmail.com>
2020-07-10 02:31:44 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -158,8 +158,11 @@ const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
 Block.findOne({id: message.author.id},(err,BlockY)=> {
-  if(BlockY.id == message.author.id)return message.react("⏪");
-  return;
+  async function a() {
+  if(BlockY.id == message.author.id){
+  message.react("⏪");
+  } 
+  }return;
 });
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
