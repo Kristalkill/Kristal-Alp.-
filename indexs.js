1bc247caf2b6fbd152bd505f54b7a682e5f1887f
Kristalkill <vovk.fox@gmail.com>
2020-07-10 01:52:24 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -107,17 +107,18 @@ const args = message.content.slice(res.Moderation.prefix.length).trim().split(/ 
 const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
-if(BlockY){
-  message.react("⏪"); 
-}
-else{
+switch(BlockY){ 
+case true:{ message.react("⏪") }break;
+case false:{
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
 if(!config.owner.includes(message.author.id)&&(!message.guild.owner.user)&&(!member.hasPermission(command.Permission)))return message.reply(ErrEmbed.setDescription(`**У вас нету прав** ${command.Permission}`));
 command.execute(Main, message, args,res,Data,err);
 }
 }
-})
+}
+}
+)
 })
 })
 })
@@ -159,17 +160,18 @@ const args = message.content.slice(res.Moderation.prefix.length).trim().split(/ 
 const cmdName = args.shift().toLowerCase();
 const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 if(!command)return;
-if(BlockY){
-  message.react("⏪"); 
-}
-else{
+switch(BlockY){ 
+case true:{ message.react("⏪") }break;
+case false:{
 if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`У бота не хватает следуйщих прав: **${command.PermissionBOT}**`))
 if(!config.owner.includes(message.author.id) && command.public === false) return;
 if(!config.owner.includes(message.author.id)&&(!message.guild.owner.user)&&(!member.hasPermission(command.Permission)))return message.reply(ErrEmbed.setDescription(`**У вас нету прав** ${command.Permission}`));
 command.execute(Main, message, args,res,Data,err);
 }
 }
-})
+}
+}
+)
 })
 })
 })
