aa0c4b29dd3633d79e9907847c54246eb446b557
Kristalkill <vovk.fox@gmail.com>
2020-07-11 02:05:30 +0300

    Heroku Exec

--- events/message.js
+++ events/message.js
@@ -6,7 +6,7 @@ module.exports = (Main,message) => {
   Guild.findOne({guildID: message.guild.id},(err,res) => {
   const prefix1 = message.guild.me;
   const prefix = message.content.startsWith(res.Moderation.prefix);
-  const args = message.content.slice(res.Moderation.prefix.length||prefix1.length + 1).trim().split(/ +/g);
+  const args = message.content.slice(res.Moderation.prefix.length).trim().split(/ +/g);
   const cmdName = args.shift().toLowerCase();
   const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
   if(err){console.log(err)};
@@ -33,7 +33,7 @@ module.exports = (Main,message) => {
     Data.level+=1
     message.channel.send(embed.setDescription(`Поздравим **${message.author.username}** с ${Data.level} уровнем!`))}
     Data.save();
-    if(!prefix && !prefix1)return;
+    if(!prefix)return;
     const cooldown = cooldowns.get(message.author.id);
     if (cooldown) {
         const remaining = humanizeDuration(cooldown - Date.now(),{ round: true,language: "ru"  });
