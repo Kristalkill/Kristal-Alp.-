2f602eb3d194e07c1a91de40b570ba0349b94613
Kristalkill <vovk.fox@gmail.com>
2020-07-10 23:48:33 +0300

    Heroku Exec

--- events/message.js
+++ events/message.js
@@ -1,5 +1,18 @@
 let embed = new Discord.MessageEmbed()
-managePerms = require('../functions/managePerms.js')
+function managePerms(message, needPerms, addMore = false) {
+  let need = [];
+  if (addMore) {
+    needPerms.push("EMBED_LINKS");
+    needPerms.push("ADD_REACTIONS");
+    needPerms.push("USE_EXTERNAL_EMOJIS");
+  }
+  needPerms.map((p) => !message.channel.permissionsFor(addMore ? message.guild.me : message.member).has(p) ? need.push(p) : null);
+  if (need.length) return {
+    need,
+    embed: message.channel.permissionsFor(message.guild.me).has("EMBED_LINKS") ? true : false
+  };
+  else return false;
+}
 module.exports = (Main,message) => {
   if(message.author.bot)return;
   Block.findOne({id: message.author.id},(err,BlockY)=> {
