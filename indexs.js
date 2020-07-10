45382a01e23ca6419c0b8f23d695f0530fa2a423
Kristalkill <vovk.fox@gmail.com>
2020-07-10 20:34:16 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -37,6 +37,16 @@ fs.readdirSync('./commands').forEach(module => {
         Main.commands.set(command.name, command);
     }
 })
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error;
+  files.forEach(file => {
+    if (!file.endsWith('.js')) return;
+    const event = require(`./events/${file}`);
+    let evtName = file.split('.')[0];
+    console.log(`Загружен ивент '${evtName}'`);
+    Main.on(evtName, event.bind(null, Main));
+  });
+});
 Main.on("guildMemberAdd", async (member,guild) => {
   if(member.user.bot)return;
   let user = new User({guildID:member.guild.id, userID:member.user.id})
@@ -71,56 +81,6 @@ Main.on('guildCreate', guild => {
 })
 Main.on('guildDelete', guild => {
 Guild.deleteOne({guildID: guild.id})
-})
-Main.on('messageUpdate', async(oldMsg, message) => {
-  if(message.author.bot)return;
-  Block.findOne({id: message.author.id},(err,BlockY)=> {
-  User.findOne({guildID: message.guild.id, userID: message.author.id},(err,Data)=> {
-  Guild.findOne({guildID: message.guild.id},(err,res) => {
- if(err){console.log(err);}
- if(!Data){
-    let user = new User({guildID:message.guild.id, userID:message.author.id})
-      user.save()
- }
-  if(!res){
-      let guild = new Guild({guildID: message.guild.id,ownerID:message.guild.ownerid})
-      guild.save()
-    }
-if(Data&&res){
-Data.xp += res.Economy.xp
-Data.money += res.Economy.money
-Data.massages++
-addAchievement(Data.level >= 5,'3',Data,message)
-addAchievement(Data.money >= 1000,'2',Data,message)
-if(Data.xp >= res.Economy.upXP*Data.level){
-Data.xp -= res.Economy.upXP*Data.level;
-Data.level+=1
-let embed = new Discord.MessageEmbed()
-.setDescription(`Поздравим **${message.author.username}** с ${Data.level} уровнем!`);
-message.channel.send(embed);
-}
-Data.save()
-if(message.guild.member(message.mentions.users.first()) == message.guild.me &&  !message.content.startsWith(res.Moderation.prefix)){
-  let BotEmbed = new Discord.MessageEmbed()
-  .setTitle(`**Префикс бота:** ${res.Moderation.prefix}`);
-  message.channel.send(BotEmbed)
- }
-if(!message.content.startsWith(res.Moderation.prefix))return;
-const args = message.content.slice(res.Moderation.prefix.length).trim().split(/ +/g);
-const cmdName = args.shift().toLowerCase();
-const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
-if(!command)return;
-if(BlockY)return message.react("⏪");
-if(!BlockY){
-if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`**К сожелению у бота нету прав:` `\`${command.PermissionBOT}\` Я не могу исполнить вашу команду.`));
-if(!config.owner.includes(message.author.id) && command.public === false) return;
-if(!config.owner.includes(message.author.id)&&(!message.guild.owner.user)&&(!member.hasPermission(command.Permission)))return message.reply(ErrEmbed.setDescription(`**К сожелению у вас нету прав:` `\`${command.Permission}\` Я не могу исполнить вашу команду.`));
-command.execute(Main, message, args,res,Data,err);
-}
-}
-})
-})
-})
 })
 Main.on('message', async(message) => {  
   if(message.author.bot)return;
