48ca8e9effe9b12ff1b0c72aeca1e15f380aeb75
Kristalkill <vovk.fox@gmail.com>
2020-07-11 22:39:35 +0300

    Heroku Exec

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,30 +1,28 @@
 let embed = new Discord.MessageEmbed()
 let embed1 = new Discord.MessageEmbed()
-module.exports = (Main, message) => {
+module.exports = (Main,message) => {
+  if(!message.guild.me.hasPermission(["ADD_REACTIONS","VIEW_CHANNEL","SEND_MESSAGES","USE_EXTERNAL_EMOJIS"]))return message.reply(ErrEmbed.setDescription(`**К сожелению у бота нету прав:  \`${command.PermissionBOT}\`\nЯ не могу исполнить вашу команду.**`));
   if(message.author.bot)return;
   Block.findOne({id: message.author.id},(err,BlockY)=> {
   User.findOne({guildID: message.guild.id, userID: message.author.id},(err,Data)=> {
   Guild.findOne({guildID: message.guild.id},(err,res) => {
-  const prefix1 = message.content.startsWith(`<@!704604456313946182>`) 
-  const prefix = message.content.startsWith(res.Moderation.prefix);
-  const args = message.content.slice(prefix).trim().split(/ +/g);
+  var prefixes = [`${message.guild.me}`,`${res.Moderation.prefix}`]
+  let prefix = false;
+  for (const thisPrefix of prefixes) {
+    if (message.content.toLowerCase().startsWith(thisPrefix)) prefix = thisPrefix;
+}
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmdName = args.shift().toLowerCase();
   const command = Main.commands.get(cmdName) || Main.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
   if(err){console.log(err)};
-  if(!Data){
-    let user = new User({guildID:message.guild.id, userID:message.author.id})
-    user.save()
-     }
-   if(!res){
-    let guild = new Guild({guildID: message.guild.id,ownerID:message.guild.ownerid})
-    guild.save()
-   }
-   if(BlockY && command){ 
+  if(!Data) return User.create({guildID:message.guild.id, userID:message.author.id});
+  if(!res) return Guild.create({guildID: message.guild.id,ownerID:message.guild.ownerid});
+  if(BlockY && command){ 
    message.react("⏪");}
    else if(Data && res){
     Data.xp += res.Economy.xp
     Data.money += res.Economy.money
-    Data.messages++
+    Data.massages++
     addAchievement(Data.level >= 5,'3',Data,message)
     addAchievement(Data.money >= 1000,'2',Data,message)
     if(Data.xp >= res.Economy.upXP*Data.level){
@@ -32,20 +30,20 @@ module.exports = (Main, message) => {
     Data.level+=1
     message.channel.send(embed.setDescription(`Поздравим **${message.author.username}** с ${Data.level} уровнем!`))}
     Data.save();
-    if(message.guild.member(message.mentions.users.first()) == message.guild.me && !prefix){
-        message.channel.send(embed1.setTitle(`**Префикс бота:** ${res.Moderation.prefix}`))};
-    if(!prefix)return;
+    if(prefix && command){
     const cooldown = cooldowns.get(message.author.id);
     if (cooldown) {
         const remaining = humanizeDuration(cooldown - Date.now(),{ round: true,language: "ru"  });
         return message.channel.send(ErrEmbed.setDescription(`Подождите ${remaining} прежде чем использывть снова`))}
-    if(!command)return;
-    if(!config.owner.includes(message.author.id)&&!message.guild.owner.user != message.author.id){
+    if(!config.owner.includes(message.author.id)){
+    if(!message.member.hasPermission(command.Permission))return message.reply(ErrEmbed.setDescription(`**К сожелению у вас нету прав: \`${command.Permission}\`\nЯ не могу исполнить вашу команду.**`));
     cooldowns.set(message.author.id, Date.now() + 5000);
-    setTimeout(() => cooldowns.delete(message.author.id), 5000)
-    if(!member.hasPermission(command.Permission))return message.reply(ErrEmbed.setDescription(`**К сожелению у вас нету прав:` `\`${command.Permission}\` Я не могу исполнить вашу команду.`));}
-    if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`**К сожелению у бота нету прав:` `\`${command.PermissionBOT}\` Я не могу исполнить вашу команду.`));
-    command.execute(Main, message, args,res,Data,err);
+    setTimeout(() => cooldowns.delete(message.author.id), 5000);
+    };
+    if(!message.guild.me.hasPermission(command.PermissionBOT))return message.reply(ErrEmbed.setDescription(`**К сожелению у бота нету прав:  \`${command.PermissionBOT}\`\nЯ не могу исполнить вашу команду.**`));
+    command.execute(Main, message, args,res,Data,err);}
+    else if(message.content.startsWith(message.guild.me)&& !command){
+    message.channel.send(embed1.setTitle(`**Префикс бота:** ${res.Moderation.prefix}`));}
 }
 })
 })
