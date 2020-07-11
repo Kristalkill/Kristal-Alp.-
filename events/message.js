7e477296aabb57b3232e93785f131078a4d4afff
Kristalkill <vovk.fox@gmail.com>
2020-07-11 15:01:09 +0300

    Heroku Exec

--- events/message.js
+++ events/message.js
@@ -47,7 +47,7 @@ module.exports = (Main,message) => {
         if(!message.guild.me.hasPermission(command.PermissionBOT))return message.guild.owner.send(ErrEmbed.setDescription(`**К сожелению у бота нету прав:` `\`${command.PermissionBOT}\` Я не могу исполнить вашу команду.`));
     command.execute(Main, message, args,res,Data,err);}
     if(message.mentions.users.first() == message.guild.me && !command){
-    message.channel.send(embed1.setTitle(`**Префикс бота:** ${res.Moderation.prefix}`));}
+    message.channel.send(embed.setTitle(`**Префикс бота:** ${res.Moderation.prefix}`));}
 })
 })
 })
