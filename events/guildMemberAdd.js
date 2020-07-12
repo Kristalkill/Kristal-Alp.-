692d9964ec18fe47d44d4f77d6fea7abc0748cf3
Kristalkill <vovk.fox@gmail.com>
2020-07-12 11:36:57 +0300

    Heroku Exec

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = (Main,member,guild) => {
 if(member.user.bot)return;
-User.create({guildID:member.guild.id, userID:member.author.id});
+User.create({guildID:guild.id, userID:member.user.id});
 }
