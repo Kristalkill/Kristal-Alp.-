cef925af089597cfb76d0b4e990a814702c6cca2
Kristalkill <vovk.fox@gmail.com>
2020-07-14 22:35:48 +0300

    Heroku Exec

--- commands/moder/clear.js
+++ commands/moder/clear.js
@@ -13,8 +13,8 @@ if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!');
 if (amount > 100) return message.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
 if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
 
-await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
-    msg.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
+await message.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
+    message.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
 )});
     }
 }
