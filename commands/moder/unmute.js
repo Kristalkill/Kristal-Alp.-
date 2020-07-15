6f74983142789ac21e0bfcab7afe34a6cedc88ff
Kristalkill <vovk.fox@gmail.com>
2020-07-15 12:22:19 +0300

    Heroku Exec

---
+++ commands/moder/unmute.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "unmute",
+    description: "unmute",
+    Permission:["MANAGE_CHANNELS"],
+    PermissionBOT:["MANAGE_CHANNELS","MANAGE_ROLES"],
+    aliases: [],
+    public: true,
+    async execute(Main, message, args) {
+Guild.findOne({guildID: message.guild.id},(err,res) => {
+let muterole = (res.Moderation.muterole ||message.guild.roles.cache.find(x => /(В)?[Mм][uyу][t(ьт)]([eеd])?/gi.test(x.name)).id);
+let member = message.guild.member(message.mentions.users.filter(u=>!u.bot).first()||message.guild.members.get(args[0]))
+if(member.roles.cache.includes(muterole)){
+  member.roles.remove(muterole);
+  message.channel.send(OKEmbed.setDescriptiom(`${member} розмучен!`));
+}
+})
+}
+}
