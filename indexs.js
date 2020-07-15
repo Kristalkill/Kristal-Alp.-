804e5e00e987163c827c1aa930fca3411f337151
Kristalkill <vovk.fox@gmail.com>
2020-07-15 15:58:21 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -52,11 +52,11 @@ Mute.find().sort('userID').exec((err,res)=> {
   res.forEach(async mute => {
     const guild = Main.guilds.cache.get(mute.guildid)
     console.log(guild)
+    if(!guild)return;
     const role = guild.role.cache.get(mute.role);
     if(!guild.members.cache.get(mute.id) && mute.time !== null && mute.time <= Date.now()) res.deleteOne({guild: mute.guildid,id:mute.id});
     if(!guild.members.cache.get(mute.id))return;
     if(!role) res.deleteOne({guild: mute.guildid,id:mute.id})
-    if(!guild)return;
     if(mute.time === null){
       if(!guild.members.cache.get(mute.id).roles.cache.has(mute.role)) guild.memebrs.cache.get(mute.id).roles.add(mute.role)
 
