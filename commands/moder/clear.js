1cf2315a7a5b973db0d979e2ad057890484a6a8c
Kristalkill <vovk.fox@gmail.com>
2020-07-15 09:53:10 +0300

    Heroku Exec

--- commands/moder/clear.js
+++ commands/moder/clear.js
@@ -8,8 +8,6 @@ module.exports = {
     async execute(Main, message, args,res) {
 const amount = args[0];
 if (isNaN(amount)||1 < amount > 100)return message.reply(ErrEmbed.setDescription(`Введите число от 1 до 100`))
-await message.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
-    message.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-)});
+    message.channel.bulkDelete(amount).then(messages => message.reply(OKEmbed.setDescription(`Удалено ${messages.size} messages`)))
     }
 }
