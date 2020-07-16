4f76765e544d6d563599eec1244fe91783787d81
Kristalkill <vovk.fox@gmail.com>
2020-07-16 16:53:55 +0300

    Heroku Exec

--- commands/development/block.js
+++ commands/development/block.js
@@ -5,7 +5,7 @@ module.exports = {
     public: false,
     async execute(Main, message, args) {
     const member =  message.guild.member(message.mentions.users.filter(u=>!u.bot).first()  || message.guild.members.cache.get(args[0]))
-    const reason = args[1].toLowercase()||"неизвесной"
+    const reason = args[1].toLowerCase()||"неизвесной"
     Block.findOne({id: member.id},(err,Data)=> {
     if(!Data)return Block.create({id:member.id,reason:reason})
     message.reply(`${member} заблокирован по причине **${reason}**`);
