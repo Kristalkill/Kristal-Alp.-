acb30a364ebe747973cd31371627166bb2cbf632
Kristalkill <vovk.fox@gmail.com>
2020-07-17 12:12:30 +0300

    Heroku Exec

--- commands/development/block.js
+++ commands/development/block.js
@@ -4,8 +4,8 @@ module.exports = {
     aliases: [],
     public: false,
     async execute(Main, message, args) {
-    const member =  message.guild.member(message.mentions.users.filter(u=>!u.bot).first()  || message.guild.members.cache.get(args[0]))
-    const reason = args[1].toLowerCase()||"неизвесной"
+    const member =  message.guild.member(message.mentions.users.filter(u=>!u.bot).first())
+    const reason = args.toLowerCase()||"неизвесной"
     Block.findOne({id: member.id},(err,Data)=> {
     if(!Data){
     Block.create({id:member.id,reason:reason})
