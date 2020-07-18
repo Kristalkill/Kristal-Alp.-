7b604323e8e3410ae04302736d3c2b72a7e95f0a
Kristalkill <vovk.fox@gmail.com>
2020-07-18 14:06:54 +0300

    Heroku Exec

--- commands/development/block.js
+++ commands/development/block.js
@@ -6,10 +6,11 @@ module.exports = {
     async execute(Main, message, args) {
     const member =  message.guild.member(message.mentions.users.filter(u=>!u.bot).first())
     const reason = args.join(" ").toLowerCase()||"неизвесной"
+    if(!member)return;
     Block.findOne({id: member.id},(err,Data)=> {
     if(!Data){
     Block.create({id:member.id,reason:reason})
-    message.channel.send(ErrEmbed.setDescription(`${member} заблокирован по причине **${reason}**`));
+    message.channel.send(OKEmbed.setDescription(`${member} заблокирован по причине **${reason}**`));
     }
     else return message.channel.send(ErrEmbed.setDescription(`${member} уже заблокирован по причине ${Data.reason}`))
 })
