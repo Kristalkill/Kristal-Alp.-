448a47673a28c6373087251d2f1f37708e38039d
Kristalkill <vovk.fox@gmail.com>
2020-07-19 10:12:39 +0300

    Heroku Exec

---
+++ commands/economy/shop.js
@@ -0,0 +1,43 @@
+const ErrEmbed = require("../../embeds/ErrEmbed");
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+module.exports = {
+    name: 'shop',
+    description: 'shop',
+    PermissionBOT:["MANAGE_ROLES"],
+    aliases: [],
+    public: true,
+    async execute(Main, message, args) {
+Guild.findOne({guildID: message.guild.id},async(err,res) => {
+User.findOne({guildID:member.guild.id, userID:member.id},async(err,Data) => {
+shop = res.Economy.shop
+role = message.guild.roles(args[2]||message.mentions.roles.first())
+var role = {
+    name = role.name,
+    id  = role.id,
+    cost = parseInt(args[3])
+}
+switch(true){
+case args[1] == `add` && message.author.hasPermission("ADMINISTRATOR"):
+if(parseInt(args[3]) > 0){
+Object.assign(shop,role)
+}else{
+message.channel.send(ErrEmbed.setDescription(`Минимальная цена 1$`))
+}
+break;
+case args[1] == `delete` && message.author.hasPermission("ADMINISTRATOR"):
+res.Economy.shop.getElementById(role.id).delete()
+break;
+case shop.forEach(element => {elemend.id == args[1] && element.cost < Data.money }):
+await message.author.roles.add(muterole.id);
+break;
+default:
+}})})}}
--- models/guild.js
+++ models/guild.js
@@ -6,6 +6,7 @@ const guildchema = mongoose.Schema({
      muterole:{ type: String, default: "0"},
    },
     Economy:{
+      shop:Object,
       Partner:{
          price:{type:Number,default:5000},
          level:{type:Number,default:5},
