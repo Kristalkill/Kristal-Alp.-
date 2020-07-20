28f72c060ecf552b5448c471bd1f7c432712ea54
Kristalkill <vovk.fox@gmail.com>
2020-07-20 15:33:57 +0300

    Heroku Exec

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -9,16 +9,17 @@ module.exports = {
 Guild.findOne({guildID: message.guild.id},async(err,res) => {
 User.findOne({guildID:message.guild.id, userID:message.author.id},async(err,Data) => {
 shop = res.Economy.shop
-role = message.mentions.roles.first() || message.guild.roles.cache.get(args[2])
-var role = {
+role = (message.mentions.roles.first() || message.guild.roles.cache.get(args[2]));
+var roles = {
     name:role.name,
     id:role.id,
     cost:parseInt(args[3])
 }
+var addrole = [role.id,role.name,parseInt(args[3])]
 switch(true){
 case args[1] == `add` && message.author.hasPermission("ADMINISTRATOR"):
 if(parseInt(args[3]) > 0){
-Object.assign(shop,role)
+Object.assign(shop,roles)
 }else{
 message.channel.send(ErrEmbed.setDescription(`Минимальная цена 1$`))
 }
@@ -26,7 +27,7 @@ break;
 case args[1] == `delete` && message.author.hasPermission("ADMINISTRATOR"):
 res.Economy.shop.getElementById(role.id).delete()
 break;
-case shop.forEach(element => {elemend.id == args[1] && element.cost < Data.money }):
+case shop.entries(element => {elemend.id == args[1] && element.cost < Data.money }):
 await message.author.roles.add(muterole.id);
 break;
 default:
