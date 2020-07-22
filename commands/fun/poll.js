7b508d59f50196be1e3abce7c695d813e4dfc716
Kristalkill <vovk.fox@gmail.com>
2020-07-22 10:46:40 +0300

    Heroku Exec

--- commands/fun/poll.js
+++ commands/fun/poll.js
@@ -1,15 +1,13 @@
 const { DiscordAPIError } = require("discord.js");
-
-let embed = new Discord.MessageEmbed()
-.setTitle(`ГОЛОСОВАНИЕ`)
-.setFooter(message.author.tag)
-.setDescription
 module.exports = {
     name: 'poll',
     description: 'poll',
     aliases: ["poll"],
     public: true,
     async execute(Main, message, args) {
+        let embed = new Discord.MessageEmbed()
+.setTitle(`ГОЛОСОВАНИЕ`)
+.setFooter(message.author.tag)
         if (!args) return message.reply(ErrEmbed.setDescription("Напишите за что голосовать!"))
         const pollTopic = await message.channel.send(embed.setDescription(message.content.slice(2)));
         await pollTopic.react(`✅`);
