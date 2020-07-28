615d8409fa8aed4e9e9b4b917ac48915bcd6babb
Kristalkill <vovk.fox@gmail.com>
2020-07-28 14:02:25 +0300

    Heroku Exec

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -1,5 +1,4 @@
 const ErrEmbed = require("../../embeds/ErrEmbed");
-const { Guild } = require("discord.js");
 module.exports = {
     name: 'shop',
     description: 'shop',
@@ -11,11 +10,16 @@ Guild.findOne({guildID: message.guild.id},async(err,res) => {
 User.findOne({guildID:message.guild.id, userID:message.author.id},async(err,Data) => {
 shop = res.Economy.shop
 role = (message.mentions.roles.first() || message.guild.roles.cache.get(args[2]));
-var addrole = [role.id,role.name,parseInt(args[3])]
 switch(true){
 case args[1] == `add` && message.author.hasPermission("ADMINISTRATOR"):
 if(parseInt(args[3]) > 0){
-Object.assign(shop,addrole)
+if(shop.hasOwnProperty(role.id)){
+    message.channel.send(`Роль уже есть в магазине`)
+}else{
+shop[role.id] = {
+    price:parseInt(args[3])
+}
+}
 message.channel.send('Роль успешно добавлена в магазин')
 }else{
 message.channel.send(ErrEmbed.setDescription(`Минимальная цена 1$`))
@@ -23,6 +27,7 @@ message.channel.send(ErrEmbed.setDescription(`Минимальная цена 1$
 break;
 case args[1] == `delete` && message.author.hasPermission("ADMINISTRATOR"):
 res.Economy.shop.getElementById(role.id).delete()
+message.channel.send('Роль успешно удалена из магазина')
 break;
 default:
 }})
