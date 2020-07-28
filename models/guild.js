105f2ddd7d9a631f928a89701bd069a6ab20576a
Kristalkill <vovk.fox@gmail.com>
2020-07-28 22:12:15 +0300

    Heroku Exec

--- models/guild.js
+++ models/guild.js
@@ -6,7 +6,7 @@ const guildchema = mongoose.Schema({
      muterole:{ type: String, default: "0"},
    },
     Economy:{
-      shop:{type:Object,default:{}},
+      shop:{type:Object,default:null},
       Partner:{
          price:{type:Number,default:5000},
          level:{type:Number,default:5},
