17fd326dfb13ee13393ccd4a15e82544dc092cc6
Kristalkill <vovk.fox@gmail.com>
2020-07-28 20:47:53 +0300

    Heroku Exec

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -7,19 +7,33 @@ module.exports = {
     public: true,
     async execute(Main, message, args) {
 Guild.findOne({guildID: message.guild.id},(err,res) => {
-    const role = (message.mentions.roles.first() || message.guild.roles.cache.get(args[0]));
-    if(parseInt(args[1]) > 0){
-        if(Object.getOwnPropertyNames(res.Economy.shop).includes(role.id)){
-            message.channel.send(`Роль уже есть в магазине`)
-        }else{
-           res.Economy.shop[role.id] = parseInt(args[3])
-           res.save();
-           message.channel.send('Роль успешно добавлена в магазин')
+const role = (message.mentions.roles.first() || message.guild.roles.cache.get(args[1]));
+if(message.member.hasPermission("ADMINISTRATOR")){
+if(role){
+if(args[0] == 'add'){
+        if(parseInt(args[2]) > 0){
+            if(Object.getOwnPropertyNames(res.Economy.shop).includes(role.id)){
+                message.channel.send(`Роль уже есть в магазине`)
+            }else{
+               res.Economy.shop[role.id] = parseInt(args[2])
+               res.save();
+               message.channel.send('Роль успешно добавлена в магазин')
+            }
+            }else{
+            message.channel.send(ErrEmbed.setDescription(`Минимальная цена 1$`))
+            }
+        }else if(args[0] == 'delete'){
+            if(Object.getOwnPropertyNames(res.Economy.shop).includes(role.id)){
+            Object.getOwnPropertyNames(res.Economy.shop).find(role.id).then(role1 =>{delete role1})
+            message.channel.send('Роль успешно удалена из магазина')
+            }else{
+                message.channel.send('Роли нету в магазине')
+            }
         }
-        }else{
-        message.channel.send(ErrEmbed.setDescription(`Минимальная цена 1$`))
-        }
-        
+}else{
+message.channel.send(ErrEmbed.setDescription(`Укажите роль`))
+}
+}
 })
     }
 }
