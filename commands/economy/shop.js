eabead2c6879d6904b7f50460214348094ef3826
Kristalkill <vovk.fox@gmail.com>
2020-07-28 14:18:40 +0300

    Heroku Exec

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -8,13 +8,13 @@ module.exports = {
     async execute(Main, message, args) {
 Guild.findOne({guildID: message.guild.id},async(err,res) => {
     const shop = res.Economy.shop
-    const role = (message.mentions.roles.first() || message.guild.roles.cache.get(args[2]));
+    const role = (message.mentions.roles.first() || message.guild.roles.cache.get(args[1]));
     if(parseInt(args[2]) > 0){
         if(!shop.hasOwnProperty(role.id)){
             message.channel.send(`Роль уже есть в магазине`)
         }else{
         shop[role.id] = {
-            price:parseInt(args[3])
+            price:parseInt(args[2])
         }
         }
         message.channel.send('Роль успешно добавлена в магазин')
