7241d265de098a18ab85dec30a07929a25627c87
Kristalkill <vovk.fox@gmail.com>
2020-07-28 13:50:21 +0300

    Heroku Exec

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,4 @@
 module.exports = (Main,guild) => {
 if(!guild.me.hasPermission(["ADD_REACTIONS","VIEW_CHANNEL","SEND_MESSAGES","USE_EXTERNAL_EMOJIS"]))return message.guild.owner.send(ErrEmbed.setDescription(`**К сожелению у бота нету прав:  \`${["ADD_REACTIONS","VIEW_CHANNEL","SEND_MESSAGES","USE_EXTERNAL_EMOJIS"]}\`\nЯ не могу исполнить вашу команду.**`));
-Guild.create({guildID: message.guild.id,ownerID:message.guild.ownerid});
+Guild.create({guildID: guild.id,ownerID:guild.ownerid});
 }
