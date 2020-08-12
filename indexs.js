5e6906ba48ffda1e61afb352a77c9d4bdc185b4c
Kristalkill <vovk.fox@gmail.com>
2020-08-13 01:03:37 +0300

    Heroku Exec

--- indexs.js
+++ indexs.js
@@ -8,25 +8,12 @@ global.fs = require("fs");
 global.mongoose = require("mongoose");
 ////____FUNCTIONS___///
 require("dotenv").config();
-let locate = (fullkey, obj) => {
-  let keys = fullkey.split(".");
-  let val = obj[keys.shift()];
-  if (!val) return null;
-  for (let key of keys) {
-      if (!val[key]) return val;
-      val = val[key];
-      if (Array.isArray(val)) return val.join("\n");
-  }
-  return val || null;
-};
-String.prototype.parse = function (options = {}) {
-  if (!this) return this;
-  return this.split(" ").map(str => str.replace(/\%(.+)\%/gi, (matched, key) => locate(key, options) || matched)).join(" ");
-};
 String.prototype.translate = function(vars){
+  var str = this;
   for (const [KEY, value] of Object.entries(vars)) {
-      return this.replace(/\%(.*?+)\%/gi,value)
+    str = str.replace(new RegExp(`%${KEY}%`, 'g'), value);
   }
+  return str;
 }
 PORT = process.env.PORT || 4000
 addAchievement = require('./functions/addAchievement.js')
