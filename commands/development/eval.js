d3c6500174d22b084b6847a8976dea8e6842f98c
Kristalkill <vovk.fox@gmail.com>
2020-08-13 16:52:37 +0300

    Heroku Exec

--- commands/development/eval.js
+++ commands/development/eval.js
@@ -15,7 +15,7 @@ module.exports = {
       .addField('Выход',`\`\`\`js\nType: ${tyype}\nDone for: ${new Date().getTime() - message.createdTimestamp + 'ms'}\`\`\``,true)
       message.channel.send(embed).then((message) => {message.react("✅")
       for(i= 1024;i <= evaled.match(/.{1,1024}/g);i + 1024){
-        let evaled = await evaled.match(/.{1,i}/g);
+        let evaled = evaled.match(/.{1,i}/g);
         embed[i] = new Discord.MessageEmbed();
         message.channel.send(embed[i].setDescription(`\`\`\`${evaled}\`\`\``))
       }
