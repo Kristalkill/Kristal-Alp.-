d11d329244091d856ac964e4fb18926d69a508d2
Kristalkill <vovk.fox@gmail.com>
2020-08-13 22:53:26 +0300

    Heroku Exec

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -6,11 +6,10 @@ module.exports = {
     public: true,
     async execute(Main, message, args) {
       try {
-        const duration = moment.duration(Main.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
         const Botinfoembed = new Discord.MessageEmbed()
             .setTitle("**Показатели бота**")
             .setColor("RANDOM")
-            .addField(`**Техническая**`, `>>> **<:cpu:709750871692542142> | CPU:**  ${(process.cpuUsage().user/1024/1024/100).toFixed(2)}%\n**<:ram:709751455610961972> | RAM:**  ${(process.memoryUsage().heapUsed / (1000 * 1000)).toFixed(2)} MB\n**🕑 | Uptime:**  ${duration}\n**⚙ | Кол-во команд:**  ${Main.commands.size}\n**💡 | Discord.js:**  v${version}`, true)
+            .addField(`**Техническая**`, `>>> **<:cpu:709750871692542142> | CPU:**  ${(process.cpuUsage().user/1024/1024/100).toFixed(2)}%\n**<:ram:709751455610961972> | RAM:**  ${(process.memoryUsage().heapUsed / (1000 * 1000)).toFixed(2)} MB\n**🕑 | Uptime:**  ${humanizeDuration(Main.uptime,{ round: true,language: "ru"})}\n**⚙ | Кол-во команд:**  ${Main.commands.size}\n**💡 | Discord.js:**  v${version}`, true)
             .addField("**👥 | Социальная**", `>>> **:man_artist_tone3:Пользователей**  ${Main.users.cache.size}\n**🌐 | Серверов:**  ${Main.guilds.cache.size}\n**🗨 | Каналов:**  ${Main.channels.cache.size}`, true)
             .setFooter("Автор команды: END#3123");
           message.channel.send(Botinfoembed)
