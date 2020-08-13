f2dfb0be0a83592c117a0980faa0dc3f7734f65f
Kristalkill <vovk.fox@gmail.com>
2020-08-13 16:26:04 +0300

    Heroku Exec

--- commands/development/eval.js
+++ commands/development/eval.js
@@ -10,18 +10,16 @@ module.exports = {
       const tyype = typeof evaled[0].toUpperCase() + typeof evaled.slice(1);
       if(typeof evaled!== 'string') evaled = require('util').inspect(evaled, {depth: 0});
       evaled == (undefined || null) ? evaled = 'Empty response: ' + evaled : evaled;
-      for(i= 2048;i <= evaled.match(/.{1,1024}/g);i * 2){
-        let evaled = await eval(argss).match(/.{1,i}/g);
+      let embed = new Discord.MessageEmbed()
+      .addField('Вход',`\`\`\`js\n${argss}\`\`\``)
+      .addField('Выход',`\`\`\`js\nType: ${tyype}\nDone for: ${new Date().getTime() - message.createdTimestamp + 'ms'}\`\`\``,true)
+      message.channel.send(embed).then((message) => {message.react("✅")
+      for(i= 2048;i <= evaled.match(/.{1,1024}/g);i + 1024){
+        let evaled = await evaled.match(/.{1,i}/g);
         embed[i] = new Discord.MessageEmbed();
         message.channel.send(embed[i].setDescription(`\`\`\`${evaled}\`\`\``))
       }
-      if(evaled.substr().length >= 1024 ){
-        
-      }
-let embed = new Discord.MessageEmbed()
-.addField('Вход',`\`\`\`js\n${argss}\`\`\``)
-.addField('Выход',`\`\`\`js\nType: ${tyype}\nDone for: ${new Date().getTime() - message.createdTimestamp + 'ms'}\n${evaled}\`\`\``,true)
-message.channel.send(embed).then((embed) => message.react("✅"))
+    })
 } catch(err) {
 let embed = new Discord.MessageEmbed()
 .addField('Вход',`${argss}`)
