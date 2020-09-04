a78513e57667e681dc6bbf8dc581a8abff91dcc5
Kristalkill <vovk.fox@gmail.com>
2020-09-04 16:47:47 +0300

    Heroku Exec

--- src/Structures/Shards.js
+++ src/Structures/Shards.js
@@ -1,4 +1,4 @@
 const {ShardingManager} = require('discord.js');
-const manager = new ShardingManager('./src/index.js', { token: process.env.token,  totalShards: 'auto',});
+const manager = new ShardingManager('./src/index.js', { token: process.env.token,  totalShards: 'auto',autoSpawn: true});
 manager.on('shardCreate', shard => console.log(`[Shard Loading]: Шард по айди: #${shard.id} запускается...`));
-manager.spawn(2,5500,400000);
+manager.spawn();
