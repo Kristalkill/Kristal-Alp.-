2db8aec4c60ea76a45a98d89921c6b8922b86bc6
Kristalkill <vovk.fox@gmail.com>
2020-09-04 17:12:49 +0300

    Heroku Exec

---
+++ src/Commands/info/shards.js
@@ -0,0 +1,23 @@
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['shards'],
+			description: 'shards',
+			category: 'info'
+		});
+	}
+	async run(message,language) {
+const values = await this.Main.shard.broadcastEval(`
+ [
+ this.shard.id,
+ this.guilds.size
+ ]
+`);
+let finalString = "**Статус шарда**\n\n"
+values.forEach((value) => {
+ finalString += `• ШАРД#${value[0]}|Сервера: ${value[1]}\n`;
+});
+message.channel.send(finalString);
+    }
+}
