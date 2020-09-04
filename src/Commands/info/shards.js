22bb9e0b6f7496d4cc5451551cffe8494450ffe1
Kristalkill <vovk.fox@gmail.com>
2020-09-04 17:39:36 +0300

    Heroku Exec

--- src/Commands/info/shards.js
+++ src/Commands/info/shards.js
@@ -11,13 +11,13 @@ module.exports = class extends Command {
 	}
 	async run(message,language) {
         let embed = new MessageEmbed().setTitle(`ШАРДЫ`)
-	    const uptime = await client.shard.broadcastEval('this.uptime');
-		const ping = await client.shard.broadcastEval('Math.round(this.ws.ping)');
-		const ram = await client.shard.broadcastEval(`process.memoryUsage().rss`);
-		const guilds = await client.shard.fetchClientValues('guilds.cache.size');
-		const users = await client.shard.fetchClientValues('users.cache.size')
-        for (let i = 0; i < client.options.shardCount; i++) {
-            embed.addField(`${i}:${client.util.parseDur(uptime[i])}~${Math.round(ping[i])}ms,${this.Main.utils.formatBytes(ram[i])} ${guilds[i]}, ${users[i]}`)
+	    const uptime = await this.Main.shard.broadcastEval('this.uptime');
+		const ping = await this.Main.shard.broadcastEval('Math.round(this.ws.ping)');
+		const ram = await this.Main.shard.broadcastEval(`process.memoryUsage().rss`);
+		const guilds = await this.Main.shard.fetchClientValues('guilds.cache.size');
+		const users = await this.Main.shard.fetchClientValues('users.cache.size')
+        for (let i = 0; i < this.Main.options.shardCount; i++) {
+            embed.addField(`${i}:${this.Main.util.parseDur(uptime[i])}~${Math.round(ping[i])}ms,${this.Main.utils.formatBytes(ram[i])} ${guilds[i]}, ${users[i]}`)
         }
         message.channel.send(embed)
     }
