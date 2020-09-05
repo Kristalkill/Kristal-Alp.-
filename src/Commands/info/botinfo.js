0ac2c09fbfa9ee14a921ed023c1ddc8949885063
Kristalkill <vovk.fox@gmail.com>
2020-09-05 16:18:59 +0300

    Heroku Exec

--- src/Commands/info/botinfo.js
+++ src/Commands/info/botinfo.js
@@ -19,31 +19,25 @@ module.exports = class extends Command {
           this.Main.shard.fetchClientValues('users.cache.size'),
 
       ];
-       Promise.all(promises).then(async results => { 
+       Promise.all(promises).then(results => { 
         let Botinfoembed = new Discord.MessageEmbed()
-        const CPU = await results[0].reduce((var1, var2) => var1 + var2, 0);
-        const RAM = await results[1].reduce((var1, var2) => var1 + var2, 0);
-        const Channels = await results[2].reduce((var1, var2) => var1 + var2, 0);
-        const Servers = await results[3].reduce((var1, var2) => var1 + var2, 0);
-        const Emojis = await results[4].reduce((var1, var2) => var1 + var2, 0);
-        const Users = await results[5].reduce((var1, var2) => var1 + var2, 0);
-        await message.channel.send(Botinfoembed
+        message.channel.send(Botinfoembed
           .setTitle("**Показатели бота**")
           .setColor("RANDOM")
-          .setThumbnail(await message.guild.iconURL())
+          .setThumbnail(message.guild.iconURL())
           .addField(
-            `**Техническая**`, `>>> **<:cpu:709750871692542142> | CPU:** ${CPU.toFixed(2)}%
-            **<:ram:709751455610961972> | RAM:**  ${this.Main.utils.formatBytes(RAM)} 
+            `**Техническая**`, `>>> **<:cpu:709750871692542142> | CPU:** ${results[0].reduce((var1, var2) => var1 + var2, 0).toFixed(2)}%
+            **<:ram:709751455610961972> | RAM:**  ${this.Main.utils.formatBytes(results[1].reduce((var1, var2) => var1 + var2, 0))} 
             **🕑 | Uptime:**  ${humanizeDuration(this.Main.uptime,{ round: true,language: "ru"})}
             **⚙ | Кол-во команд:**  ${this.Main.commands.size}
             **💡 | Discord.js:**  v${Discord.version}
             **Discord API:** ${new Date().getTime() - message.createdTimestamp}ms
             **Bot Ping:** ${Math.round(this.Main.ws.ping)}ms.`, true)
           .addField(
-          `**👥 | Социальная**`, `>>> **:man_artist_tone3:Пользователей**  ${Users}
-          **🌐 | Серверов:**  ${Servers}
-          **🗨 | Каналов:**  ${Channels}
-          **🤣 | Емодзи:**  ${Emojis}`, true))
+          `**👥 | Социальная**`, `>>> **:man_artist_tone3:Пользователей**  ${results[5].reduce((var1, var2) => var1 + var2, 0)}
+          **🌐 | Серверов:**  ${results[3].reduce((var1, var2) => var1 + var2, 0)}
+          **🗨 | Каналов:**  ${results[2].reduce((var1, var2) => var1 + var2, 0)}
+          **🤣 | Емодзи:**  ${results[4].reduce((var1, var2) => var1 + var2, 0)}`, true))
       });
       } catch (error) {
         console.log(error)
