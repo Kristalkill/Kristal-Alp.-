e1caf4f23775dded067af26cd3bfeb5e67f80201
Kristalkill <vovk.fox@gmail.com>
2020-09-06 10:38:51 +0300

    Heroku Exec

--- src/Structures/Main.js
+++ src/Structures/Main.js
@@ -40,7 +40,7 @@ module.exports = class Main extends Discord.Client {
 		this.db.User = require('../models/user');
 
 		this.nodes = [
-			{ id: "1", host: "localhost", port: process.env.PORT, password: process.env.PASS}
+			{ id: "1", host: "localhost", port: process.env.PORT, password: "enderman"}
 		  ];
 		this.music =  new Manager(this,this.nodes)
 	}
