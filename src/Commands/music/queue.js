c0448267653e50db27502eacb99a6212255e3093
Kristalkill <vovk.fox@gmail.com>
2020-09-09 18:28:54 +0300

    Heroku Exec

---
+++ src/Commands/music/queue.js
@@ -0,0 +1,18 @@
+const Command = require('../../Structures/Command');
+const humanizeDuration = require('humanize-duration')
+module.exports = class extends Command {
+	constructor(...args) {
+		super(...args, {
+		});
+	}
+	async run(message,language,args) {
+        let data = ''
+        let i = 0
+        this.Main.music.players.get('564403545273663489').queue.tracks.forEach(e => {
+            const { title, length, uri } = this.Main.utils.decode(e.song)
+            data += `[${i + 1}] [${title}](${uri}) - ${humanizeDuration(length,{round: true,language: res.Moderation.language})}\n`;
+        });
+        message.channel.send(data)
+
+    }
+}
