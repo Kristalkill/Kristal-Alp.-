804c19445a7659d4f2b759cfea85a307bfa3b879
Kristalkill <vovk.fox@gmail.com>
2020-09-09 19:54:01 +0300

    Heroku Exec

--- src/Commands/music/queue.js
+++ src/Commands/music/queue.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
         const tracks = await this.Main.music.players.get(message.guild.id).queue.tracks
         const track  = await this.Main.utils.decode(this.Main.music.players.get(message.guild.id).queue.current.song)
         data += `[${i}] [${track.title}](${track.uri}) - ${humanizeDuration(track.length,{round: true,language: message.guild.settings.Moderation.language})}\n`;
-        if(tracks){
+        if(tracks.length > 0){
         tracks.forEach(async e => {
             const { title, length, uri } = await this.Main.utils.decode(e.song)
             data += `[${i + 1}] [${title}](${uri}\'${title}\') - ${humanizeDuration(length,{round: true,language: message.guild.settings.Moderation.language})}\n`;
