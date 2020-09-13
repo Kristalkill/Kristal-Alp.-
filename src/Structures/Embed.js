23a56fe315451b1cec4c53fa333b8a675cfeb280
Kristalkill <vovk.fox@gmail.com>
2020-09-13 20:01:29 +0300

    Heroku Exec

--- src/Structures/Embed.js
+++ src/Structures/Embed.js
@@ -2,19 +2,25 @@ const {MessageEmbed} = require ('discord.js');
 const space = "\u200b"
 
 module.exports = class Embed extends MessageEmbed {
-
-
-    chunk(title,content){
+    constructor(){
+        super()
+    }
+    async chunk(title,content){
         content.toString()
-        if(content.length < 1024) return this.fields.push({name:title,value:content,inline:false})
-        if (1024 < content.length  && content.length < 2048)return this.description = content;
-         if(content.length > 2048){
+        if(content.length < 1024){
+        this.fields.push({name:title,value:content,inline:false})
+        }
+        if (1024 < content.length  && content.length < 2048) 
+        {
+        this.description = content;
+        }
+        else if(content.length > 2048){
             this.fields.push({name : title,value:content.slice(0,content.length),inline:false})
             content = content.slice(1024)
             content.chunk(1024).forEach(element => {
             this.fields.push({name:space,value:element,inline:false})
         });
         }
-        return this;
+        return await this;
     }
 }
