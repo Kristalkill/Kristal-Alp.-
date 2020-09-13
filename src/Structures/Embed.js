d49321864a587eb58dc40f88dccf08c4b028663b
Kristalkill <vovk.fox@gmail.com>
2020-09-13 20:29:40 +0300

    Heroku Exec

--- src/Structures/Embed.js
+++ src/Structures/Embed.js
@@ -4,22 +4,19 @@ const space = "\u200b"
 module.exports = class Embed extends MessageEmbed {
 
 
-    async chunk(title,content){
-        content.toString()
+    chunk(title,content){
         if(content.length < 1024){
         this.fields.push({name:title,value:content,inline:false})
-        }
-        else if (1024 < content.length  && content.length < 2048) 
+        }else if (1024 < content.length  && content.length < 2048) 
         {
         this.description = content;
-        }
-        else if(content.length > 2048){
+        }else if(content.length > 2048){
             this.fields.push({name : title,value:content.slice(0,content.length),inline:false})
             content = content.slice(1024)
             content.chunk(1024).forEach(element => {
             this.fields.push({name:space,value:element,inline:false})
         });
         }
-        return await this;
+        return this;
     }
 }
