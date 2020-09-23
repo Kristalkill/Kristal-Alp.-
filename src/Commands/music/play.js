1fffdfd7ec5adebee92048ce269ba25dc5969dcb
Kristalkill <vovk.fox@gmail.com>
2020-09-23 22:18:35 +0300

    Heroku Exec

--- src/Commands/music/play.js
+++ src/Commands/music/play.js
@@ -50,62 +50,70 @@ module.exports = class extends Command {
     const searchData = await node.rest.resolve(query, 'youtube');
     if (!searchData.tracks.length)
       return await message.channel.send(language.play.params.param2);
-    let e = new MessageEmbed().setTitle(
-      language.play.params.param5.translate({ query: query })
-    );
-    const tracks = searchData.tracks.splice(10);
+    const tracks = searchData.tracks.slice(0, 10);
     let i = 1;
-    e.description = '';
-    tracks.forEach((x) => {
-      e.description += `[${i++}] [${x.info.title}](${
-        x.uri
-      }) - ${humanizeDuration(x.info.length, {
-        round: true,
-        language: message.guild.settings.Moderation.language,
-      })}\n`;
-    });
-    message.channel.send(e).then(async (m) => {
-      const numbers = {
-        '1️⃣': 0,
-        '2️⃣': 1,
-        '3️⃣': 2,
-        '4️⃣': 3,
-        '5️⃣': 4,
-        '6️⃣': 5,
-        '7️⃣': 6,
-        '8️⃣': 7,
-        '9️⃣': 8,
-      };
-      const reacted = await this.Main.utils.Rcollector(
-        await this.Main.utils.reaction(
-          ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'],
+    let data = '';
+    for (const x of tracks) {
+      data += `[${i++}] [${x.info.title}](${x.info.uri}) - ${humanizeDuration(
+        x.info.length,
+        {
+          round: true,
+          language: message.guild.settings.Moderation.language,
+        }
+      )}\n`;
+    }
+    message.channel
+      .send(
+        new MessageEmbed()
+          .setTitle(language.play.params.param5.translate({ query: query }))
+          .setDescription(data)
+      )
+      .then(async (m) => {
+        const numbers = {
+          '1️⃣': 0,
+          '2️⃣': 1,
+          '3️⃣': 2,
+          '4️⃣': 3,
+          '5️⃣': 4,
+          '6️⃣': 5,
+          '7️⃣': 6,
+          '8️⃣': 7,
+          '9️⃣': 8,
+          '🔟': 9,
+        };
+        const reacted = await this.Main.utils.Rcollector(
+          await this.Main.utils.reaction(
+            ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'].slice(
+              0,
+              tracks.length
+            ),
+            m,
+            true
+          ),
           m,
-          true
-        ),
-        m,
-        message.author,
-        60000,
-        1
-      );
-      reacted.on('collect', async (reaction) => {
-        let b = numbers[reaction.emoji.name];
-        const track = searchData.tracks[b];
-        const res = await this.Main.music.queue.handle(node, track, message);
-        await message.channel
-          .send(
-            language.play.params.param4.translate({ track: track.info.title })
-          )
-          .catch(() => null);
-        if (res) await res.play();
+          message.author,
+          60000,
+          1
+        );
+        reacted.on('collect', async (reaction) => {
+          let b = numbers[reaction.emoji.name];
+          const track = searchData.tracks[b];
+          const res = await this.Main.music.queue.handle(node, track, message);
+          await message.channel
+            .send(
+              language.play.params.param4.translate({ track: track.info.title })
+            )
+            .catch(() => null);
+          if (res) await res.play();
+        });
+        reacted.on('end', (collected, reason) => {
+          if (reason == 'limit') {
+            m.delete();
+          } else
+            return m.edit(
+              new MessageEmbed().setTitle(language.play.params.param6)
+            );
+        });
       });
-      reacted.on('end', (collected, reason) => {
-        if (reason == 'limit') {
-          m.delete();
-        } else
-          return m.edit(
-            new MessageEmbed().setTitle(language.play.params.param6)
-          );
-      });
-    });
   }
 };
