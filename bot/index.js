18ce87316f32cb851adddf4ff7de7d7ceb042f27
Kristalkill <vovk.fox@gmail.com>
2021-04-30 15:49:05 +0300

    Heroku Exec

--- bot/index.js
+++ bot/index.js
@@ -53,7 +53,7 @@ function send(eror, promise) {
     code: eror.code,
     method: eror.method
   };
-  return Main.channels.cache.get('772907942718341130').send(new MessageEmbed().setDescription(`message: ${error.message}`).setTitle(error.name).setURL(error.path || promise).setTimestamp()
+  return Main.channels.cache.get('709749244931866669').send(new MessageEmbed().setDescription(`message: ${error.message}`).setTitle(error.name).setURL(error.path || promise).setTimestamp()
     .setFooter(error.code + error.method))
 }
 process.on('unhandledRejection', (reason, promise) => {
@@ -67,7 +67,7 @@ process.on('uncaughtException', (err, origin) => {
 });
 process.on('uncaughtExceptionMonitor', (err, origin) => {
   let args = [err, origin]
-  Main.channels.cache.get('772907942718341130').send(new MessageEmbed().setDescription(`message: ${args.map((arg) => JSON.parse(JSON.stringify(arg)))}`));
+  Main.channels.cache.get('709749244931866669').send(new MessageEmbed().setDescription(`message: ${args.map((arg) => JSON.parse(JSON.stringify(arg)))}`));
 })
 process.on('warning', (warning) => {
   send(warning)
